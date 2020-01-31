f1dc3a35740e39c08d81af02bd70f23ff1ca6892
Enaho Murphy <enahomurphy@gmail.com>
2020-01-31 13:25:33 +0100

    Move api url env variable

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -1,7 +1,9 @@
 import ApolloClient, {InMemoryCache} from 'apollo-boost';
+import {API_URL} from 'react-native-dotenv';
 
+console.log(API_URL);
 const client = new ApolloClient({
-  uri: 'http://192.168.8.52:4000/graphql',
+  uri: API_URL,
   cache: new InMemoryCache().restore({}),
 });
 
